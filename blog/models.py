a0fcf78569bd1b7d591cfb7c236f930f4aed07da
Ruon90 <143106386+Ruon90@users.noreply.github.com>
2025-11-12 11:19:28 +0000

    Update models.py with new post values

--- blog/models.py
+++ blog/models.py
@@ -11,3 +11,5 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     STATUS = ((0, "Draft"), (1, "Published"))
     status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
